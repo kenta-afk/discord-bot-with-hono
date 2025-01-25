921c6ff6c6bbb779174fb6d6654b838f519c33f7
土居健太郎 <kentarou9.60@icloud.com>
2025-01-25 16:35:25 +0900

    整理

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import { helpCommand } from './commands/helpCommand';
 import { deleteSelfComponent } from './components/deleteSelfComponent';
 import { DiscordHono } from 'discord-hono';
 
+
 const app = new DiscordHono()
   .command('hello', helloCommand)
   .command('help', helpCommand)
-  .component('delete-self', deleteSelfComponent);
+  .component('delete-self', deleteSelfComponent)
+  
+
+
 
 export default app;
